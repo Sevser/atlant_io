4c82c6b9f27932c0d0e712074e3c26d19522967a
Semyon Evstigneev <semyon-evstigneev@yandex.ru>
2022-01-29 18:59:59 +0300

    Updates

---
+++ precache-manifest.6792b339a207d407f988e5b3c8722d80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6099c1271306da166f",
+    "url": "/atlant_io/css/app.611fa648.css"
+  },
+  {
+    "revision": "07c03e43a45e5e4c48b0",
+    "url": "/atlant_io/css/chunk-05fe5c2e.6b617e2d.css"
+  },
+  {
+    "revision": "ce85e756d4ead5de3ea8",
+    "url": "/atlant_io/css/chunk-1c2ad13e.2a6d403d.css"
+  },
+  {
+    "revision": "735a32bf4b1532114bec287ad31f3c07",
+    "url": "/atlant_io/index.html"
+  },
+  {
+    "revision": "dd6099c1271306da166f",
+    "url": "/atlant_io/js/app.498fbf0b.js"
+  },
+  {
+    "revision": "07c03e43a45e5e4c48b0",
+    "url": "/atlant_io/js/chunk-05fe5c2e.c269eccd.js"
+  },
+  {
+    "revision": "ce85e756d4ead5de3ea8",
+    "url": "/atlant_io/js/chunk-1c2ad13e.b297262d.js"
+  },
+  {
+    "revision": "6816aa4ba60c53db90a9",
+    "url": "/atlant_io/js/chunk-vendors.6dd4d6c5.js"
+  },
+  {
+    "revision": "173a423f1037e79fbf6353f28c975301",
+    "url": "/atlant_io/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/atlant_io/robots.txt"
+  }
+]);
